e1cd87583b6f2b1411bdf2a473f357836bf7d64a
Leonardo Marcos <leonardo.marcos@cwi.com.br>
2024-02-07 23:35:31 -0300

    Create structure of screens

---
+++ src/screens/index.ts
@@ -0,0 +1,3 @@
+export {WordsScreen} from './words/words.screen';
+export {HistoryScreen} from './history/history.screen';
+export {FavoritesScreen} from './favorites/favorites.screen';
---
+++ src/screens/words/words.screen.tsx
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from 'react';
+import {View} from 'react-native';
+import wordsData from '../../core/words';
+
+import style from './words.style';
+import {WordsList} from '@components';
+
+const itensPerPage = 15;
+
+export function WordsScreen() {
+  const [words, setWords] = useState<string[]>([]);
+  const [page, setPage] = useState(1);
+
+  const loadMoreItems = () => {
+    const nextItems = wordsData.slice(words.length, page * itensPerPage);
+
+    setWords(prevWords => {
+      setPage(previousPage => previousPage + 1);
+
+      return [...prevWords, ...nextItems];
+    });
+  };
+
+  useEffect(() => {
+    loadMoreItems();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <View style={style.container}>
+      <WordsList onLoadMoreItems={loadMoreItems} words={words} />
+    </View>
+  );
+}
