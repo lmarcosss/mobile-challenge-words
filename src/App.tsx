5e949c1f25344071121b1941e33412ac512438a5
Leonardo Marcos <leonardo.marcos@cwi.com.br>
2024-02-09 09:24:53 -0300

    Put word details context provider in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {TabNavigation} from './navigation/tab-navigation';
+import {TabNavigator} from './navigation/tab-navigator';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {WordDetailsModalScreen} from '@screens';
+import {WordDetailsContextProvider} from '@contexts';
+
+const RootStack = createNativeStackNavigator();
 
 export function App() {
   return (
     <NavigationContainer>
       <SafeAreaProvider>
-        <TabNavigation />
+        <WordDetailsContextProvider>
+          <RootStack.Navigator>
+            <RootStack.Group>
+              <RootStack.Screen
+                name="TabNavigator"
+                options={{headerShown: false}}
+                component={TabNavigator}
+              />
+              <RootStack.Screen
+                name="WordDetailsModal"
+                component={WordDetailsModalScreen}
+                options={{
+                  presentation: 'modal',
+                }}
+              />
+            </RootStack.Group>
+          </RootStack.Navigator>
+        </WordDetailsContextProvider>
       </SafeAreaProvider>
     </NavigationContainer>
   );
--- src/screens/history/history.screen.tsx
+++ src/screens/history/history.screen.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
+import {WordsList} from '@components';
+import {useWordDetailsContext} from '@contexts';
+
+import style from './history.style';
 
 export function HistoryScreen() {
+  const {history} = useWordDetailsContext();
+
   return (
-    <View>
-      <Text>HistoryScreen</Text>
+    <View style={style.container}>
+      <WordsList words={history} />
     </View>
   );
 }
